ef0de43ff95c4bb5538e70163ef3178a3bdee0f2
YahirMaster777 <yahirperez141002@gmail.com>
2024-09-17 09:46:46 -0600

    Se elimino la rama secundaria de la practica 05, tanto localmente como remotamente

--- DartPad/Excercise_03_Dynamic.dart
+++ DartPad/Excercise_03_Dynamic.dart
@@ -23,7 +23,17 @@ void main(){
   errorMessage = true;
    print(""" ---------------------------------------------------
   El nuevo valor de errorMessage es: $errorMessage, 
-  en este casoi fue asignado con el tipo boolenao""");
+  en este caso fue asignado con el tipo boolenao""");
 
+   errorMessage = [400,404,500,502,505];
+   print(""" ---------------------------------------------------
+  El nuevo valor de errorMessage es: $errorMessage, 
+  en este de que se registren varios errores y los pone en una lista""");
+
+
+   errorMessage = {404,"Error de red", true, -1054.23,"idgs"};
+   print(""" ---------------------------------------------------
+  El nuevo valor de errorMessage es: $errorMessage, 
+  Puede ser un conjunto de datos (SET)""");
   //Marvin Yair Tolentino Perez 210540
 }
