b6e839d3da8b235daaa94dd16e3ffc6dc07faa7d
YahirMaster777 <yahirperez141002@gmail.com>
2024-09-27 11:27:45 -0600

    se agrego el formato para que la fecha saliera con el formato dd/mm/aa

--- DartPad/Excercise_08_clases_p.dart
+++ DartPad/Excercise_08_clases_p.dart
@@ -1,5 +1,5 @@
 void main() {
-  // Creación de una instancia de la clase Person con valores estaticos
+  // Creación de una instancia de la clase Person con valores estáticos
   final juanOliver = Person(
     ID: 1,
     name: 'Juan',
@@ -8,13 +8,16 @@ void main() {
     gender: 'Masculino',
     bloodGroup: 'O+',
     curp: 'OJPJ850607HDFLRN01',
-    birthdate: DateTime(1985, 6, 7),
+    birthdate: DateTime(2002, 6, 7),
     isActive: true,
     createdAt: DateTime(2023, 1, 1),
     updatedAt: DateTime(2023, 8, 1),
   );
 
-  // impresion de los detalles de la persona
+  // Impresión de los detalles de la persona
+  print("--------------------------------------------");
+  print("Datos de la persona");
+  print("--------------------------------------------");
   print('ID: ${juanOliver.ID}');
   print('Nombre: ${juanOliver.name}');
   print('Apellido paterno: ${juanOliver.firstLastname}');
@@ -22,10 +25,14 @@ void main() {
   print('Género: ${juanOliver.gender}');
   print('Grupo sanguíneo: ${juanOliver.bloodGroup}');
   print('CURP: ${juanOliver.curp}');
-  print('Fecha de nacimiento: ${juanOliver.birthdate}');
+  print('Fecha de nacimiento: ${formatDate(juanOliver.birthdate)}');
   print('Activo: ${juanOliver.isActive}');
-  print('Creado en: ${juanOliver.createdAt}');
-  print('Última actualización: ${juanOliver.updatedAt}');
+  print('Registtrado: ${formatDate(juanOliver.createdAt)}');
+}
+
+/// Función para formatear la fecha en el formato dd/mm/aa
+String formatDate(DateTime date) {
+  return '${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year.toString().substring(2)}';
 }
 
 /// Clase de la tabla persona de la base de datos
@@ -45,7 +52,6 @@ class Person {
 
   /// Constructor de la clase Person
   /// Si `createdAt` no se proporciona, se asigna la fecha y hora actuales
-  
   Person({
     required this.ID,
     required this.name,
@@ -55,7 +61,7 @@ class Person {
     required this.bloodGroup,
     this.curp,
     required this.birthdate,
-    this.isActive = true,  // Valor por defecto: true
+    this.isActive = true, // Valor por defecto: true
     DateTime? createdAt,
     this.updatedAt,
   }) : createdAt = createdAt ?? DateTime.now();
